1d290716321c672f18a4f154c71857286b9ef888
jdchavarro <jd-chavarro@hotmail.com>
2020-10-20 22:16:13 -0500

    Base de MVC para iniciar

---
+++ Library/Conexion.php
@@ -0,0 +1,10 @@
+<?php
+
+class Conexion {
+
+    public $db;
+
+    function __construct() {
+        $this->db = new QueryManager("localhost", "root", "toor", "crediapp");
+    }
+}
---
+++ Library/QueryManager.php
@@ -0,0 +1,15 @@
+<?php
+
+class QueryManager {
+
+    private $link;
+
+    function __construct($host, $user, $password, $dbname) {
+
+        $this->link = new mysqli($host, $user, $pass, $dbname);
+
+        if ($this->link->connect_errno) {
+            echo "Error de Conexion: " . $this->link->connect_error;
+        }
+    }
+}
